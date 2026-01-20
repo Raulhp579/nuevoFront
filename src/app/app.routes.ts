31ae9e342f081d811db255cb1a57fb70bfb688e1
Raulhp579 <raulhenares5@gmail.com>
2026-01-20 20:43:57 +0100

    arreglado listo para validaciones

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,16 @@ import { Header } from './components/header/header';
 import { Home } from './components/home/home';
 import { Fichajes } from './components/fichajes/fichajes';
 import { User } from './components/user/user';
+import { Login } from './components/login/login';
+import { Register } from './components/register/register';
+
 
 export const routes: Routes = [
     {path:'header', component:Header},
     {path:'home', component:Home},
     {path:'',redirectTo:'home',pathMatch:'full'},
     {path:'fichajes',component:Fichajes},
-    {path:'user', component:User}
+    {path:'user', component:User},
+    {path:'login',component:Login},
+    {path:'register',component:Register}
 ];
--- src/app/components/login/login.ts
+++ src/app/components/login/login.ts
@@ -4,10 +4,11 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-login',
-  imports: [MatCardModule, MatFormFieldModule, MatInputModule, MatButtonModule, MatIconModule],
+  imports: [MatCardModule, MatFormFieldModule, MatInputModule, MatButtonModule, MatIconModule, RouterLink],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
