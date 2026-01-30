da0219a2dc6d561597e7d6820254258bcb50bc21
Raulhp <raulhenares5@gamil.com>
2026-01-30 22:11:37 +0100

    feat: Implement user authentication including login and registration components, service, and related project configuration.

--- src/app/services/auth-service.ts
+++ src/app/services/auth-service.ts
@@ -6,37 +6,37 @@ import { Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class AuthService {
-  private enlace:string = "http://127.0.0.1:8000/api"
+  private enlace: string = 'http://127.0.0.1:8000/api';
 
-  private getHeaders():HttpHeaders{
+  private getHeaders(): HttpHeaders {
     return new HttpHeaders({
-      'Content-Type':'application/json'
-    })
+      'Content-Type': 'application/json',
+    });
   }
 
-  private getHeadersConToken():HttpHeaders{
-    const token = sessionStorage.getItem("token")
+  private getHeadersConToken(): HttpHeaders {
+    const token = sessionStorage.getItem('token');
     return new HttpHeaders({
-      'Content-Type':'application/json',
-      /* 'Authorization': `Bearer ${token}` */
-    })
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    });
   }
 
-  constructor(private http:HttpClient){}
+  constructor(private http: HttpClient) {}
 
-  register(user:any):Observable<any>{
-    return this.http.post(`${this.enlace}/register`,user,{headers:this.getHeaders()})
+  register(user: any): Observable<any> {
+    return this.http.post(`${this.enlace}/register`, user, { headers: this.getHeaders() });
   }
 
-  login(user:any):Observable<any>{
-    return this.http.post(`${this.enlace}/login`,user,{headers:this.getHeaders()})
+  login(user: any): Observable<any> {
+    return this.http.post(`${this.enlace}/login`, user, { headers: this.getHeaders() });
   }
 
-  logOut():Observable<any>{
-    return this.http.delete(`${this.enlace}/logOut`,{headers:this.getHeadersConToken()})
+  logOut(): Observable<any> {
+    return this.http.delete(`${this.enlace}/logOut`, { headers: this.getHeadersConToken() });
   }
 
-  user():Observable<any>{
-    return this.http.get(`${this.enlace}/userInfo`,{headers:this.getHeadersConToken()})
+  user(): Observable<any> {
+    return this.http.get(`${this.enlace}/userInfo`, { headers: this.getHeadersConToken() });
   }
 }
